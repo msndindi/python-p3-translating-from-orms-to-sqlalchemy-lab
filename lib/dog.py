d719b94673b8ffdf4742b6329c43d0ff51381683
Mutindi Hope <msndindi@outlook.com>
2023-05-30 08:45:14 +0300

    Completed Assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,37 @@
 from models import Dog
 
-def create_table(base):
-    pass
+## Don't need "if __name__ == '__main__':" this time, but leaving it in for later reference##
+if __name__ == '__main__':
+    ## Don't need "engine = create_engine('sqlite:///dog.db')" this time, (BECAUSE THEY PUT IT IN THE TESTS???) but leaving it in for later reference##
+    engine = create_engine('sqlite:///dog.db')
+    base.metadata.create_all(engine)
+	## Don't need Session this time, (BECAUSE THEY PUT IT IN THE TESTS???) but leaving it in for later reference##
+    Session = sessionmaker(bind=engine)
+	## Don't need session this time, (BECAUSE THEY PUT IT IN THE TESTS???) but leaving it in for later reference##
+    session = Session()
+
+def create_table(base, engine):
+    ## Don't need "engine = create_engine('sqlite:///dog.db')" this time, (BECAUSE THEY PUT IT IN THE TESTS???) but commenting it below for later reference##
+    # engine = create_engine('sqlite:///dog.db')
+    base.metadata.create_all(engine)
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
 
 def get_all(session):
-    pass
+    return session.query(Dog).all()
 
 def find_by_name(session, name):
-    pass
+    return session.query(Dog).filter(Dog.name == name).first()
 
 def find_by_id(session, id):
-    pass
+    return session.query(Dog).filter(Dog.id == id).first()
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    return session.query(Dog).filter(Dog.name == name and Dog.breed == breed).first()
 
 def update_breed(session, dog, breed):
-    pass
+    dog.breed = breed
+    session.add(dog)
+    session.commit()
--- lib/models.py
+++ lib/models.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
 
-from sqlalchemy import (Column, String, Integer)
+## Don't need PrimaryKeyConstraint this time, but leaving it in for later reference##
+from sqlalchemy import (PrimaryKeyConstraint, Column, String, Integer)
 from sqlalchemy.ext.declarative import declarative_base
+## Don't need sessionmaker this time, (BECAUSE THEY PUT IT IN THE TESTS???) but leaving it in for later reference##
+from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
 class Dog(Base):
-    __tablename__ = 'dogs'
+    __tablename__ = "dogs"
+
+    # Index('index_name', 'name')  ## Don't need it this time ##
 
     id = Column(Integer(), primary_key=True)
     name = Column(String())
     breed = Column(String())
+
+    ## Not needed, but useful to have humans able to read the data ##
+    def __repr__(self):
+        return f"Dog ID: {self.id} " \
+            + f"Dog Name: {self.name} " \
+            + f"Dog Breed: {self.breed}"
+
+
+
